a5a65fbc8d49650c3a30c618c2c400c466fd3958
kmangutov <kmangutov@gmail.com>
2016-01-24 19:27:32 -0600

    commet data.js

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,20 +1,58 @@
 var data = {
- "name": "flare",
- "children": [
-  {
-   "name": "analytics",
-   "children": [
-      {
-       "name": "cluster",
-       "children": [
-        {"name": "AgglomerativeCluster", "size": 3938},
-        {"name": "CommunityStructure", "size": 3812},
-        {"name": "HierarchicalCluster", "size": 6714},
-        {"name": "MergeEdge", "size": 743}
-       ]
-      },
-    ]
-  }]
+  "name": "root",
+  "children": 
+  [
+    {
+      "name": "Criminal Law",
+      "children": 
+      [
+        [
+          {"name": "AgglomerativeCluster", "size": 3938},
+          {"name": "CommunityStructure", "size": 3812},
+          {"name": "HierarchicalCluster", "size": 6714},
+          {"name": "MergeEdge", "size": 743}
+        ]
+      ]
+    },
+    {
+      "name": "Civil Law",
+      "children": 
+      [
+        [
+          {
+            "name": "Labor/Employment", 
+            "size": 3938
+            "children": 
+            [
+              {"name": "Government Regulations", "size": 3812},
+              {
+                "name": "Compensation & Benefits", 
+                "size": 3812
+                "children": 
+                [
+                  {"name": "Party was injured while on the job", "size": 6714},
+                  {"name": "Employer is denying compensation or benefits", "size": 743}
+                  {"name": "Dispute over benefits package", "size": 3812},
+                  {"name": "Violation of contract provision", "size": 3812},
+                  {"name": "Dispute over status as an employee", "size": 6714},
+                ]
+              },
+              {"name": "Leave", "size": 6714},
+              {"name": "Adverse Employment Action", "size": 743}
+              {"name": "Labor Relations", "size": 3812},
+              {"name": "Wages & Hours", "size": 3812},
+              {"name": "Safety & Health", "size": 6714},
+            ]
+          },
+          {"name": "Property", "size": 3812},
+          {"name": "Torts", "size": 3812},
+          {"name": "Corporate/Commercial Law", "size": 6714},
+          {"name": "General Private Contract Disputes", "size": 743}
+        ]
+      ]
+    },
+    
+  ]
 }
 var IssueDetail = (function(){
 
