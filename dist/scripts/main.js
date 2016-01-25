85d20d503f51e2cd6e8bdf23f38c8b7ddd196a41
kmangutov <kmangutov@gmail.com>
2016-01-24 19:42:08 -0600

    fuck

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,57 +1,50 @@
 var data = {
-  "name": "root",
+  "name": "flare",
   "children": 
   [
     {
       "name": "Criminal Law",
       "children": 
       [
-        [
-          {"name": "AgglomerativeCluster", "size": 3938},
-          {"name": "CommunityStructure", "size": 3812},
-          {"name": "HierarchicalCluster", "size": 6714},
-          {"name": "MergeEdge", "size": 743}
-        ]
+        {"name": "AgglomerativeCluster", "size": 3938},
+        {"name": "CommunityStructure", "size": 3812},
+        {"name": "HierarchicalCluster", "size": 6714},
+        {"name": "MergeEdge", "size": 743}
       ]
     },
     {
       "name": "Civil Law",
       "children": 
       [
-        [
-          {
-            "name": "Labor/Employment", 
-            "size": 3938
-            "children": 
-            [
-              {"name": "Government Regulations", "size": 3812},
-              {
-                "name": "Compensation & Benefits", 
-                "size": 3812
-                "children": 
-                [
-                  {"name": "Party was injured while on the job", "size": 6714},
-                  {"name": "Employer is denying compensation or benefits", "size": 743}
-                  {"name": "Dispute over benefits package", "size": 3812},
-                  {"name": "Violation of contract provision", "size": 3812},
-                  {"name": "Dispute over status as an employee", "size": 6714},
-                ]
-              },
-              {"name": "Leave", "size": 6714},
-              {"name": "Adverse Employment Action", "size": 743}
-              {"name": "Labor Relations", "size": 3812},
-              {"name": "Wages & Hours", "size": 3812},
-              {"name": "Safety & Health", "size": 6714},
-            ]
-          },
-          {"name": "Property", "size": 3812},
-          {"name": "Torts", "size": 3812},
-          {"name": "Corporate/Commercial Law", "size": 6714},
-          {"name": "General Private Contract Disputes", "size": 743}
-        ]
+        {
+          "name": "Labor/Employment", 
+          "children": 
+          [
+            {"name": "Government Regulations", "size": 1000},
+            {
+              "name": "Compensation & Benefits", 
+              "children": 
+              [
+                {"name": "Party was injured while on the job", "size": 100},
+                {"name": "Employer is denying compensation or benefits", "size": 150},
+                {"name": "Dispute over benefits package", "size": 135},
+                {"name": "Violation of contract provision", "size": 200},
+                {"name": "Dispute over status as an employee", "size": 135}
+              ]
+            },
+            {"name": "Leave", "size": 500},
+            {"name": "Adverse Employment Action", "size": 1000},
+            {"name": "Labor Relations", "size": 750},
+            {"name": "Wages & Hours", "size": 250},
+            {"name": "Safety & Health", "size": 250}
+          ]
+        },
+        {"name": "Property", "size": 3812},
+        {"name": "Torts", "size": 3812},
+        {"name": "Corporate/Commercial Law", "size": 6714},
+        {"name": "General Private Contract Disputes", "size": 743}
       ]
-    },
-    
+    }
   ]
 }
 var IssueDetail = (function(){
